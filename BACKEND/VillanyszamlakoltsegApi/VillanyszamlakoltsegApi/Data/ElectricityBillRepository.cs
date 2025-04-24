18fc7fa0c2bd90fdba8ccce477ef85b89d0a40c1
Péter Pocsai (UQ392J) <petipocsai60@gmail.com>
2025-04-24 22:11:26 +0200

    ElectricityBIll repository létrehzva (egyenlőre 1 számlát tárolok és azzal dolgozok)

---
+++ BACKEND/VillanyszamlakoltsegApi/VillanyszamlakoltsegApi/Data/ElectricityBillRepository.cs
@@ -0,0 +1,13 @@
+﻿using VillanyszamlakoltsegApi.Models;
+
+namespace VillanyszamlakoltsegApi.Data
+{
+    public class ElectricityBillRepository
+    {
+        ElectricityBill bill;
+        public ElectricityBillRepository() 
+        {
+            bill = new ElectricityBill();
+        }
+    }
+}
---
+++ BACKEND/VillanyszamlakoltsegApi/VillanyszamlakoltsegApi/Models/ElectricityBill.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VillanyszamlakoltsegApi.Models
+{
+    public class ElectricityBill
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public decimal UnitPrice { get; set; }
+
+        public int[] Years { get; set; }
+
+        public decimal[,] Consumptions { get; set; }
+    }
+}
